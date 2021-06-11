c7efc1b70ea96624f41e35621a674c1e6e923871
senthilkumar <senthil.veeranan@gmail.com>
2021-06-11 16:06:37 +0530

    lession 5.1 link file added

--- AnsibleTutorialpracticeYAML/5 -Using scripts for system admin tasks/5.1/user.sh
+++ AnsibleTutorialpracticeYAML/5 -Using scripts for system admin tasks/5.1/user.sh
@@ -1,7 +1,7 @@
 vi user.sh
 
 ansible all -m group -a "name=ex294 state=present" --become
-ansible all -m user -a "name=deepan password='{{'redhat_pass' | password_hash('sha512')}}' groups=ex294 uid=2021"
+ansible all -m user -a "name=deepan password='{{'redhat_pass' | password_hash('sha512')}}' groups=ex294 uid=2021" --become
 
 
 
