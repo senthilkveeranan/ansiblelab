58abae2160310dc7e21c886f367e6ec1451ece5b
senthilkumar <senthil.veeranan@gmail.com>
2021-06-11 15:39:57 +0530

    lession 5 link file added

--- AnsibleTutorialpracticeYAML/5 -Using scripts for system admin tasks/file.sh
+++ AnsibleTutorialpracticeYAML/5 -Using scripts for system admin tasks/file.sh
@@ -5,5 +5,7 @@ ansible prod -m file -a "path=/root/redhat/ex294/results mode=766 owner=ansadmin
 ansible prod -m file -a "src=/root/redhat/ex294/results dest=/root/results state=link" --become
 
 
+ansible prod -m file -a "path=/root/redhat/ex294/results mode=766group+rwx,o+rw owner=ansadmin group=ansadmin state=touch" --become
+
 chmod +x file.sh 
 ./file.sh 
